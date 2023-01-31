2c704c27d042e01f4cb9114ad0b07cb06894fc28
usamaehsan <53894527+usamaehsan@users.noreply.github.com>
2023-01-31 10:33:08 +0500

    Update download.py

--- download.py
+++ download.py
@@ -1,13 +1,9 @@
-# In this file, we define download_model
-# It runs during container build time to get model weights built into the container
-
-# In this example: A Huggingface BERT model
-
-from transformers import pipeline
+import torch
+from diffusers import StableDiffusionPipeline
+import os
 
 def download_model():
-    # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
-
+    HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
+    pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16,use_auth_token=HF_AUTH_TOKEN )
 if __name__ == "__main__":
     download_model()
